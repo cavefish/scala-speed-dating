8feb25e7c1b928c0cef5635e7ef80be5ea359e5a
cavefish <cavefish@gmail.com>
2019-10-06 19:24:46 +0200

    Finish Prime tests

--- src/problems/Prime.scala
+++ src/problems/Prime.scala
@@ -3,6 +3,12 @@ package problems
 object Prime {
   def isPrime(n: Int): Boolean = n match {
     case 2 => true
-    case _ => false
+    case _ => if (n < 2) false else hasDivider(n, 3, Math.sqrt(n).toInt)
   }
+
+  @scala.annotation.tailrec
+  def hasDivider(n: Int, divider: Int, top: Int):Boolean = if
+  (divider > top) true
+  else if (n%divider == 0) false
+  else hasDivider(n, divider+2, top)
 }
